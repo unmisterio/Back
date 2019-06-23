2a6e0bced6bdd1be6aa79c739ab46c491fedeb76
anferoar96 <afromana@unal.edu.co>
2019-06-23 10:37:10 -0500

    Solucionado CORS en Express

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const Cors = require('cors');
 const routes = require('./routes'); 
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
 const app = express();
 
+app.use(Cors());
 app.use(bodyParser.json());
 
 app.get('/targetUniversity',routes.targetUniversity.get);
